d924e5b76b9f02e00fb73276c549d69f9f160c1a
CatCfrco <z82206.cat@gmail.com>
2013-10-12 15:05:24 +0800

    texture

--- texture/texture.cpp
+++ texture/texture.cpp
@@ -69,10 +69,10 @@ void handle_draw() {
 
     glRotatef(angle,0,0,1);
     glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f,  -3.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f, -1.0f,  -3.0f);     
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  -3.0f);     
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  -3.0f);    
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f,  -5.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f, -1.0f,  -8.0f);    
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  -3.0f);    
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  -3.0f);   
     glEnd();
 
     angle += 2;
