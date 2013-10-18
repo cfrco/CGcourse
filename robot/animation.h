e8cab004a636b32534e70558dc47011e73c7d245
hwaohung <johny60207@gmail.com>
2013-10-18 20:01:36 +0800

    style ok

--- robot/animation.h
+++ robot/animation.h
@@ -26,46 +26,6 @@ GLfloat aniWalkRotation2[JOINT_LENGTH][5] = {
     {-40},
 };
 
-GLfloat gangnam_style1[JOINT_LENGTH][5] = {
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {50, 0, 15},
-    {},
-    {-45},
-};
-
-
-GLfloat gangnam_style3[JOINT_LENGTH][5] = {
-    {},
-    {},
-    {-60},
-    {80},
-    {0, 0, -70},
-    {70, 0, 20},
-    {-50, 0, 15},
-    {},
-    {45},
-    {},
-};
-
-GLfloat gangnam_style4[JOINT_LENGTH][5] = {
-    {},
-    {},
-    {-75},
-    {90, 0, 20},
-    {0, 0, -90},
-    {90, 0},
-    {},
-    {},
-    {},
-    {},
-};
-
 GLfloat gentle_man1[JOINT_LENGTH][5] = {
     {},
     {},
@@ -127,7 +87,6 @@ GLfloat style_left_leg2[][5] = {
 	{0}
 };
 
-
 GLfloat style_right_leg1[][5] = {
 	{90, 0, 20},
 	{0}
@@ -139,27 +98,33 @@ GLfloat style_right_leg2[][5] = {
 };
 ////Gangnam style
 void Gangnam_style(joint_t *joints){
-	pushRotation(&joints[JOINT_ARM_RIGHT1], &style_right_hand1[0],20);
-    
-	pushRotation(&joints[JOINT_ARM_RIGHT2], &style_right_hand2[0],10);
-    pushRotation(&joints[JOINT_ARM_RIGHT2], &style_right_hand2[1],10);
-    
-	pushRotation(&joints[JOINT_ARM_RIGHT1], &style_right_hand1[1],20);
-    
-	pushRotation(&joints[JOINT_ARM_RIGHT2], &style_right_hand2[2],10);
-    pushRotation(&joints[JOINT_ARM_RIGHT2], &style_right_hand2[3],10);
-    pushRotation(&joints[JOINT_ARM_RIGHT2], &style_right_hand2[4],1);
-    
-	pushRotation(&joints[JOINT_ARM_LEFT1], &style_left_hand1[0],20);
-    pushRotation(&joints[JOINT_ARM_LEFT1], &style_left_hand1[1],20);
-    pushRotation(&joints[JOINT_ARM_LEFT2], &style_left_hand2[0],20);
-    pushRotation(&joints[JOINT_ARM_LEFT2], &style_left_hand2[1],20);
-	
+	for(int i = 0;i < 8;i++){
+		pushRotation(&joints[JOINT_ARM_RIGHT1], &style_right_hand1[0],20);
+    	
+		pushRotation(&joints[JOINT_ARM_RIGHT2], &style_right_hand2[0],10);
+    	pushRotation(&joints[JOINT_ARM_RIGHT2], &style_right_hand2[1],10);
+    	
+		pushRotation(&joints[JOINT_ARM_RIGHT1], &style_right_hand1[1],20);
+    	
+		pushRotation(&joints[JOINT_ARM_RIGHT2], &style_right_hand2[2],10);
+    	pushRotation(&joints[JOINT_ARM_RIGHT2], &style_right_hand2[3],10);
+    	pushRotation(&joints[JOINT_ARM_RIGHT2], &style_right_hand2[4],1);
+    	
+		pushRotation(&joints[JOINT_ARM_LEFT1], &style_left_hand1[0],20);
+    	pushRotation(&joints[JOINT_ARM_LEFT1], &style_left_hand1[1],20);
+    	pushRotation(&joints[JOINT_ARM_LEFT2], &style_left_hand2[0],20);
+    	pushRotation(&joints[JOINT_ARM_LEFT2], &style_left_hand2[1],20);
+	}
+		
     pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[0],20);
     pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[1],20);
     pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[1],20);
     pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[1],20);
     pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[0],20);
+    pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[1],20);
+    pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[0],20);
+    pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[1],20);
+    pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[1],20);
     pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[1],20);
     pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[0],20);
     pushRotation(&joints[JOINT_LEG_LEFT1], &style_left_leg1[1],20);
@@ -178,15 +143,22 @@ void Gangnam_style(joint_t *joints){
     pushRotation(&joints[JOINT_LEG_LEFT2], &style_left_leg2[1],20);
     pushRotation(&joints[JOINT_LEG_LEFT2], &style_left_leg2[1],20);
     pushRotation(&joints[JOINT_LEG_LEFT2], &style_left_leg2[1],20);
+	pushRotation(&joints[JOINT_LEG_LEFT2], &style_left_leg2[0],20);
     pushRotation(&joints[JOINT_LEG_LEFT2], &style_left_leg2[1],20);
     pushRotation(&joints[JOINT_LEG_LEFT2], &style_left_leg2[1],20);
-	
+    pushRotation(&joints[JOINT_LEG_LEFT2], &style_left_leg2[1],20);
+    pushRotation(&joints[JOINT_LEG_LEFT2], &style_left_leg2[1],20);
+    pushRotation(&joints[JOINT_LEG_LEFT2], &style_left_leg2[1],20);
 	
     pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[1],20);
     pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[1],20);
     pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[0],20);
     pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[1],20);
     pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[1],20);
+    pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[1],20);
+    pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[1],20);
+    pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[1],20);
+    pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[0],20);
     pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[1],20);
     pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[1],20);
     pushRotation(&joints[JOINT_LEG_RIGHT1], &style_right_leg1[1],20);
@@ -205,8 +177,16 @@ void Gangnam_style(joint_t *joints){
     pushRotation(&joints[JOINT_LEG_RIGHT2], &style_right_leg2[1],20);
 	pushRotation(&joints[JOINT_LEG_RIGHT2], &style_right_leg2[0],20);
     pushRotation(&joints[JOINT_LEG_RIGHT2], &style_right_leg2[1],20);
+	pushRotation(&joints[JOINT_LEG_RIGHT2], &style_right_leg2[1],20);
+    pushRotation(&joints[JOINT_LEG_RIGHT2], &style_right_leg2[1],20);
 	pushRotation(&joints[JOINT_LEG_RIGHT2], &style_right_leg2[0],20);
     pushRotation(&joints[JOINT_LEG_RIGHT2], &style_right_leg2[1],20);
+	pushRotation(&joints[JOINT_LEG_RIGHT2], &style_right_leg2[0],20);
+    pushRotation(&joints[JOINT_LEG_RIGHT2], &style_right_leg2[1],20);
+}
 
+
+
+void pushup(joint_t *joints){
 
 }
